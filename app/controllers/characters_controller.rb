ecc86cd18c2b91551adbafa19082f8e1339ecae5
Brian Anderson <bqanderson@me.com>
2016-11-24 23:50:01 -0600

    Add destroy function and new character link on list page

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -35,6 +35,13 @@ class CharactersController < ApplicationController
     end
   end
 
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+
+    redirect_to characters_path
+  end
+
   private
   def character_params
     params.require(:character).permit(:name, :bio, :str, :dex, :con, :int, :wis, :cha)
