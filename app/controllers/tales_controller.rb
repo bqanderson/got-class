3cd650582bed20c0e694420bb71257c409f9ca40
Brian Anderson <bqanderson@me.com>
2016-11-27 21:15:20 -0600

    Add epic tales functionality!

--- app/controllers/tales_controller.rb
+++ app/controllers/tales_controller.rb
@@ -1,2 +1,12 @@
 class TalesController < ApplicationController
+  def create
+    @character = Character.find(params[:character_id])
+    @tale = @character.tale.create(tale_params)
+    redirect_to character_path(@character)
+  end
+
+  private
+    def tale_params
+      params.require(:tale).permit(:player, :story)
+    end
 end
