09982cf027edf29f68d084d1ebda52254b4fe17e
Brian Anderson <bqanderson@me.com>
2016-11-28 01:15:34 -0600

    Fix tales functionality

--- app/controllers/tales_controller.rb
+++ app/controllers/tales_controller.rb
@@ -1,12 +1,12 @@
 class TalesController < ApplicationController
   def create
     @character = Character.find(params[:character_id])
-    @tale = @character.tale.create(tale_params)
+    @tale = @character.tales.create(tale_params)
     redirect_to character_path(@character)
   end
 
   private
     def tale_params
-      params.require(:tale).permit(:player, :story)
+      params.require(:tale).permit(:player, :body)
     end
 end
