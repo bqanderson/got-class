77332e66cc24a8b26a80821fbbcbc13d88d276ab
Brian Anderson <bqanderson@me.com>
2017-01-24 14:42:43 -0600

    Add delete tale feature.

--- app/controllers/tales_controller.rb
+++ app/controllers/tales_controller.rb
@@ -5,6 +5,13 @@ class TalesController < ApplicationController
     redirect_to character_path(@character)
   end
 
+  def destroy
+    @character = Character.find(params[:character_id])
+    @tale = @character.tales.find(params[:id])
+    @tale.destroy
+    redirect_to character_path(@character)
+  end
+
   private
     def tale_params
       params.require(:tale).permit(:player, :body)
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,4 @@
 class Character < ApplicationRecord
-  has_many :tales
+  has_many :tales, dependent: :destroy
   validates :str, :dex, :con, :int, :wis, :cha, presence: true
 end
